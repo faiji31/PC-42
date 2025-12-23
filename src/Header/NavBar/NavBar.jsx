f3c9bfd423b065894be098aef64c75f23e3dcfde
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-12-23 18:53:20 +0600

    4th commit

--- src/Header/NavBar/NavBar.jsx
+++ src/Header/NavBar/NavBar.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
+const navigationData = [
+  { id: 1, name: "Home", path: "/" },
+  { id: 2, name: "Features", path: "/features" },
+  { id: 3, name: "Pricing", path: "/pricing" },
+  { id: 4, name: "AI Concierge", path: "/ai-assistant" },
+  { id: 5, name: "Contact Us", path: "/contact" }
+];
+
+
 const Navbar = () => {
       return (
             <nav>
                   <ul className='flex'>
-                        <li className=' mr-10'><a href="">Home</a></li>
-                        <li className=' mr-10'><a href="">About</a></li>
-                        <li className=' mr-10'><a href="">Products</a></li>
+                   {
+                        navigationData.map(route=><li className='mr-10'><a href="">{route.name}</a></li>)
+                   }
                   </ul>
             </nav>
       );

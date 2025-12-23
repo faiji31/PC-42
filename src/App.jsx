70b16281a0be80e951818e4d0c353ee2d860c01e
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-12-23 18:42:02 +0600

    3rd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import DaisyNav from './Header/DaisyNav/DaisyNav'
+import NavBar from  './Header/NavBar/Navbar'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
 
      <header>
+          <NavBar></NavBar>
          <DaisyNav></DaisyNav>
      </header>
      <main>
---
+++ src/Header/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+      return (
+            <nav>
+                  <ul className='flex'>
+                        <li className=' mr-10'><a href="">Home</a></li>
+                        <li className=' mr-10'><a href="">About</a></li>
+                        <li className=' mr-10'><a href="">Products</a></li>
+                  </ul>
+            </nav>
+      );
+};
+
+export default Navbar;
